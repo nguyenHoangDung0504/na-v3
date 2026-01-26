25e6f2e2d921cb8889070ff43db74b0eb23663e8
nguyenHoangDung0504 <sdjklo2021@gmail.com>
2026-01-26 12:32:58 +0700

    Fixed addtional info display, update tracks

--- @src/database/storages/TrackStorage.mjs
+++ @src/database/storages/TrackStorage.mjs
@@ -56,7 +56,7 @@ export default class TrackStorage {
 			const images = line[8] ? line[8].split('/').map((col) => new Resource(...col.split('->'))) : [];
 			const audios = line[9] ? line[9].split('/').map((col) => new Resource(...col.split('->'))) : [];
 			const additionalURLs = line[10]
-				? line[10].split('/').map((col) => {
+				? line[10].split(',').map((col) => {
 						return new AddtionalURL(...col.split('::'));
 				  })
 				: [];
